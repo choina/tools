1a3927ca55f09d591f8b181e6d4500db2728b04f
choina <rafal.dziubak97@gmail.com>
2018-08-27 11:42:57 +0200

    Thunderbird app-change config to re-order folders

---
+++ thunderbirdapp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] folders = Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Thunderbird\\Profiles\\", "*.default");
+            string defaultprofile = folders[0];
+            defaultprofile += @"\prefs.js";
+            File.AppendAllLines(defaultprofile, new[] { "user_pref(\"mail.purge.ask\", false);" });
+        }
+    }
+}
